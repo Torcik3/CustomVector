a16e7d67723f9a4da76772a2c2dcc92846ea420d
Torcik3 <tciryt23@gmail.com>
2026-03-07 23:09:19 +0100

    custom vestor

---
+++ Vector.h
@@ -0,0 +1,73 @@
+#pragma once
+
+template<typename T>
+class Vector {
+public:
+    Vector()
+    {
+        ReAllec(2);
+    }
+    void PushBack(const T& value)
+    {
+        if (m_Size>=m_Capacity)
+            ReAllec(m_Capacity + m_Capacity/2);
+
+        m_Data[m_Size] = value;
+        m_Size++;
+    }
+
+    void PushBack(T&& value)
+    {
+        if (m_Size>=m_Capacity)
+            ReAllec(m_Capacity + m_Capacity/2);
+
+        m_Data[m_Size] = std::move(value);
+        m_Size++;
+    }
+
+    template<typename... Args>
+    T& EmplaceBack(Args&&... args) {
+        if (m_Size>=m_Capacity)
+            ReAllec(m_Capacity + m_Capacity/2);
+
+            m_Data[m_Size]=T(std::forward<Args>(args) ...);
+
+        return m_Data[m_Size++];
+    }
+
+const T& operator[](unsigned long long index) const
+    {
+    if (index >= m_Size)
+        {
+        //  assert
+        }
+    return m_Data[index];
+    }
+
+    T& operator[](unsigned long long index)
+    {
+        return m_Data[index];
+    }
+
+   unsigned long long Size() const { return m_Size; }
+private:
+    void ReAllec(unsigned long long newCapacity)
+    {
+        T* newBlock = new T[newCapacity];
+
+        if (newCapacity<m_Size)
+            m_Size = newCapacity;
+
+        for(unsigned long long i = 0; i < m_Size; i++)
+        {
+         newBlock[i] = std::move(m_Data[i]);
+        }
+        delete[] m_Data;
+        m_Data=newBlock;
+        m_Capacity=newCapacity;
+    }
+private:
+    T* m_Data = nullptr;
+    unsigned long long m_Size = 0;
+    unsigned long long m_Capacity = 0;
+};
---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include "Vector.h"
+
+class Panda {
+public:
+    int z;
+    double y;
+    std::string x;
+
+public:
+    Panda() {}
+    Panda(int z, double y, std::string x): z(z), y(y), x(x) {}
+};
+
+
+int main() {
+
+double a = 5;
+std::vector <double> b;
+b.push_back(a);
+std::cout << b.size() << std::endl;
+std::cout << b[0]<<std::endl;
+
+Vector<int> v;
+v.PushBack(2);
+    std::cout<<v[0]<<std::endl;
+
+
+    Vector<Panda> p;
+    p.EmplaceBack(1,2.3,"lody");
+std::cout<<p[0].z;
+    return 0;
+}
